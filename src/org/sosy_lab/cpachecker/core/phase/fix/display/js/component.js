1bcd374f3b0f4e6602d01a562d78930b25eb79b7
cxcfan <chengxi09@gmail.com>
2017-05-28 13:07:33 +0800

    Changed the layout of web display

--- src/org/sosy_lab/cpachecker/core/phase/fix/display/js/component.js
+++ src/org/sosy_lab/cpachecker/core/phase/fix/display/js/component.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
   $('.ui.dropdown').dropdown({
     on: 'click'
   });
+  $('.circular.ui.button').on('click', function() {
+    if ($(this).hasClass('active') && $('#mode_selected').text() == 'Manual') {
+      $(this).removeClass('active');
+    } else {
+      $(this).addClass('active');
+    }
+  })  
 });
+
